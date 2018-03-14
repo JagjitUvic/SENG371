3e19423391365ea204d8d963a8cf08a13b45adbc
Brendon Earl <bearl@uvic.ca>
2018-03-14 16:29:55 -0700

    Fix #3

--- Exercises/js/debugging.js
+++ Exercises/js/debugging.js
@@ -1,11 +1,15 @@
-$("#github").on("submit", function() {
-  var user = $("#user");
+$("#github").on("submit", function(event) {
+  event.preventDefault();
+  var user = $("#user").val();
 
   emptyCurrentList();
 
   // Using the github API https://developer.github.com/v3/repos/#list-user-repositories
   $.ajax({
-    url: "https://api.github.com/users/" + user + "/repos?client_id=1699520b1a1353e2d28f&client_secret=19f0b5d59aa52197d1c3e59ce8d85233738cda20",
+    url:
+      "https://api.github.com/users/" +
+      user +
+      "/repos?client_id=1699520b1a1353e2d28f&client_secret=19f0b5d59aa52197d1c3e59ce8d85233738cda20",
     success: function(repos) {
       iterateThroughData(repos);
     },
@@ -22,7 +26,7 @@ function iterateThroughData(repos) {
 }
 
 function insertHTML(repo) {
-  $("#repositories").append("<li>" + repo.title + "</li>");
+  $("#repositories").append("<li>" + repo.name + "</li>");
 }
 
 function emptyCurrentList() {
