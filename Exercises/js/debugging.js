b94e536fe7e247abc1c8e108f076050164494d1e
Gurjyot <gurjyot.g@gmail.com>
2018-03-14 14:01:38 -0700

    Did Lab exersize

--- Exercises/js/debugging.js
+++ Exercises/js/debugging.js
@@ -1,34 +1,43 @@
 $("#github").on("submit", function() {
   var user = $("#user");
-
+  // console.log(typeof(user));
   emptyCurrentList();
 
   // Using the github API https://developer.github.com/v3/repos/#list-user-repositories
-  $.ajax({
-    url: "https://api.github.com/users/" + user + "/repos?client_id=1699520b1a1353e2d28f&client_secret=19f0b5d59aa52197d1c3e59ce8d85233738cda20",
+  var a = $.ajax({
+    url: "https://api.github.com/users/" + user.val() + "/repos?client_id=1699520b1a1353e2d28f&client_secret=19f0b5d59aa52197d1c3e59ce8d85233738cda20",
     success: function(repos) {
       iterateThroughData(repos);
     },
     error: function(data) {
-      insertError();
+      insertError(data);
     }
   });
+  return false;
 });
 
 function iterateThroughData(repos) {
+  // for(i=0; i<repos.length; i++){
+  //   insertHTML(repos[i]);
+  // }
   _.forEachRight(repos, function(repo) {
     insertHTML(repo);
   });
 }
 
 function insertHTML(repo) {
-  $("#repositories").append("<li>" + repo.title + "</li>");
+  $("#repositories").append("<li>" + repo.name + "</li>");
 }
 
 function emptyCurrentList() {
   $("#repositories").empty();
 }
 
-function insertError() {
-  $("#repositories").append("<li>Uh oh, we're having a problem!</li>");
+function insertError(data) {
+  if (data.status==404){
+    $("#repositories").append("<li>Looks like that user is not found</li>");
+  }
+  else{
+    $("#repositories").append("<li>Uh oh, we're having a problem!</li>");
+  }
 }
