ba065e3828613a9c5678f84af2b02813a92112a6
dmahw <28464255+dmahw@users.noreply.github.com>
2018-03-14 07:28:34 -0700

    Done mostly lol

--- Exercises/js/debugging.js
+++ Exercises/js/debugging.js
@@ -1,6 +1,6 @@
 $("#github").on("submit", function() {
-  var user = $("#user");
-
+  var user = $("input[id=user]").val();
+  
   emptyCurrentList();
 
   // Using the github API https://developer.github.com/v3/repos/#list-user-repositories
@@ -22,7 +22,7 @@ function iterateThroughData(repos) {
 }
 
 function insertHTML(repo) {
-  $("#repositories").append("<li>" + repo.title + "</li>");
+  $("#repositories").append("<li>" + repo.name + "</li>");
 }
 
 function emptyCurrentList() {
